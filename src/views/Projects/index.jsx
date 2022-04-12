6609228ad9b507c3135081b8e163417311370e84
tmetsasi <tomas.metsasimola@gmail.com>
2022-04-12 11:00:20 +0300

    vähä tekstejä

---
+++ src/views/Projects/index.jsx
@@ -0,0 +1,15 @@
+import styled, {css} from "styled-components";
+import React from "react";
+
+const Testi = styled.div`
+    background-color: #fff;
+    padding: 2rem;
+    border: 2rem;
+    margin: 2rem;
+`;
+
+export const ProjectsView = () => { 
+    return ( 
+        <Testi/>
+    )
+};
