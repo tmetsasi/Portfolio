d5ffc34aee57a84b6f12044294ab9d1a98fa842d
tmetsasi <tomas.metsasimola@gmail.com>
2022-04-13 13:56:24 +0300

    karuselli vääntöö

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./Global.css";
 import styled from "styled-components";
 import { Header } from "./components/Header";
-import { SkillsView } from "./views/Skills";
-import { ExperienceView } from "./views/Experience";
 import { Footer } from "./components/Footer";
 import { Tools } from "./views/ToolsBar";
+import Carousel, { CarouselItem } from "./views/Carousel/Carousel";
+
 
 const MainPage = styled.div`
-  height: 200vh;
+  height: 250vh;
   width: 100vw;
   background-color: #407388;
   padding: 2rem;
@@ -18,9 +18,15 @@ function App() {
     <div>
       <Header />
       <MainPage>
-        
+        <Carousel> 
+          <CarouselItem>Kaikenlaista</CarouselItem>
+          <CarouselItem>Hyvää kamaa</CarouselItem>
+          <CarouselItem>Asiaa</CarouselItem>
+          <CarouselItem>Jotain dataa</CarouselItem>
+
+        </Carousel>
       
-        
+          
         <Tools/>
       </MainPage>
       <Footer/>
